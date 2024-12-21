682bf0eba0c32678d124cd7ed99869770e98b2e3
Tarun Nagarajan <tarun.greenville@gmail.com>
2024-12-21 15:06:10 +0530

    Solved 5. Staircase.cpp

---
+++ Algorithms/WarmUp/5. Staircase.cpp
@@ -0,0 +1,10 @@
+void staircase(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            cout << " ";
+        }
+        for (int k = 0; k < i; k++) {
+            cout << "#";
+        } cout << endl;
+    }
+}
